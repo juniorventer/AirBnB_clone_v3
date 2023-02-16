bd3e7abf971d14d8fab4d8d640fba2b24cde05fc
junior-venter <roccovent12@gmail.com>
2023-02-16 02:46:13 -0800

    AirBnB clone - RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,42 +1,50 @@
 #!/usr/bin/python3
-""" Starting the API! """
-
-from os import getenv
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
+""" Flask Application """
 from models import storage
 from api.v1.views import app_views
-
-HBNB_API_HOST = getenv("HBNB_API_HOST")
-HBNB_API_PORT = getenv("HBNB_API_PORT")
+from os import environ
+from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
-def close(self):
-    """Removes the current SQLAlchemy Session"""
+def close_db(error):
+    """ Close Storage """
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """Error handler of page not found"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
 
 
 if __name__ == "__main__":
-
-    host = '0.0.0.0'
-    port = "5000"
-
-    if not HBNB_API_HOST:
-        HBNB_API_HOST = "0.0.0.0"
-
-    if not HBNB_API_PORT:
-        HBNB_API_PORT = 5000
-
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, debug=True, threaded=True)
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
